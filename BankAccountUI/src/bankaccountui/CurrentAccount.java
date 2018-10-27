3815c6675989c549c20e29afc087b515c577e53e
Monirul Hasan <kmhasan@seu.edu.bd>
2018-10-27 12:53:45 +0600

    updated ui with list view

---
+++ BankAccountUI/src/bankaccountui/CurrentAccount.java
@@ -0,0 +1,57 @@
+package bankaccountui;
+
+import java.time.LocalDate;
+
+public class CurrentAccount {
+    private int id;
+    private Client client;
+    private double balance;
+   
+    // copy constructor
+    public CurrentAccount(CurrentAccount that) {
+        this.id = that.id;
+        this.client = new Client(that.client.getId(),
+                new Name(that.client.getName().getFirstName(),
+                    that.client.getName().getLastName()),
+                LocalDate.of(that.client.getDob().getYear(), 
+                        that.client.getDob().getMonth(), 
+                        that.client.getDob().getDayOfMonth())
+        );
+        this.balance = that.balance;
+    }
+    
+    public CurrentAccount(int id, Client client) {
+        this.id = id;
+        this.client = client;
+        balance = 0;
+    }
+
+    public CurrentAccount(int id, Client client, double balance) {
+        this.id = id;
+        this.client = client;
+        this.balance = balance;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public Client getClient() {
+        return client;
+    }
+
+    public double getBalance() {
+        return balance;
+    }
+    
+    public void withdraw(double amount) {
+        amount = amount * 1.01;
+        if (amount <= balance && amount > 0)
+            balance = balance - amount;
+    }
+    
+    public void deposit(double amount) {
+        if (amount > 0)
+            balance = balance + amount;
+    }
+}
---
+++ BankAccountUI/src/bankaccountui/CurrentAccount2.java
@@ -0,0 +1,12 @@
+package bankaccountui;
+
+public class CurrentAccount2 extends BankAccount {
+    public CurrentAccount2(int id, Client client, double balance) {
+        super(id, client, balance);
+    }
+
+    @Override
+    public void withdraw(double amount) {
+        super.withdraw(amount * 1.01);
+    }
+}
